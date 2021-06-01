0b10ebefba4cd78d27a6345afe82b55ffe0ec285
VICTOR AUGUSTO FERNANDES PEREIRA <victor.augusto@academico.ifrn.edu.br>
2021-05-31 21:54:32 -0300

    estruturando a agenda

--- agendamento_vacinacao/agendamento_vacinacao/settings.py
+++ agendamento_vacinacao/agendamento_vacinacao/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -134,3 +134,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = "/"
